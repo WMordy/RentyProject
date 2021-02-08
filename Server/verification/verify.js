bc3d0c96b6f5cca2436077ffab4b5570490fc859
Ouggadi Mohammed Aymane <58845118+MohammedAymane@users.noreply.github.com>
2021-02-08 18:05:01 +0100

    Update verify.js

--- Server/verification/verify.js
+++ Server/verification/verify.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const verifyToken = require("./verifyToken");
+const verifyToken = require("./verifytoken");
 
 router.get("/", (req, res, next) => {
   verifyToken(req, res, next);
