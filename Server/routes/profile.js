b8208f589d72bb33d8515180401a08d67e8a956c
Ouggadi Mohammed Aymane <58845118+MohammedAymane@users.noreply.github.com>
2021-03-21 13:26:06 +0100

    protection des routes

--- Server/routes/profile.js
+++ Server/routes/profile.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const Post = require("../models/Post");
 const User = require("../models/User");
-const verify = require("../verification/verifytoken");
 const verify2 = require("../verification/verifytoken2");
-const fs = require("fs");
 const jwt = require("jsonwebtoken");
 
-router.get('/', async (req,res) =>{
+router.get('/', verify2, async (req,res) =>{
     try{
         const Users = await User.find(null,{ firstName: 1, lastName: 1, phone: 1, subscribed_field: 1, avatar: 1, _id: 0});
         return res.json(Users);
@@ -18,7 +15,7 @@ router.get('/', async (req,res) =>{
 });
 
 
-router.get('/:id', async (req,res) =>{
+router.get('/:id', verify2, async (req,res) =>{
     try{
         const currentProfile = await User.findOne({
             _id: req.params.id
@@ -30,7 +27,7 @@ router.get('/:id', async (req,res) =>{
     }
 });
 
-router.put('/', async (req,res)=>{
+router.put('/', verify2, async (req,res)=>{
     try {
         const currentProfile = await User.updateOne({_id: (jwt.verify(req.header('auth-token'), require("../config/default.json").jwtSecret))._id},
         {
